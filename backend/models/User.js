a709a5a8f44fa05239805e0f704852e45bd5a9a0
Aqeel Shamsudheen <aqeelten@gmail.com>
2022-02-01 00:01:19 +0530

    backend init

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+    required: true,
+  },
+  plan: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8081;
+
+app.get("/", (req, res)=>{
+    res.send("Fliqxify backend");
+});
+
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
