11d43dc2d84d9dd17900e50fbe01b5a5f78f0962
Aqeel Shamsudheen <aqeelten@gmail.com>
2022-02-07 01:06:10 +0530

    mobile - password strength, confirm password, policy checkbox, password validation

--- mobile/lib/screens/signup.dart
+++ mobile/lib/screens/signup.dart
@@ -12,10 +12,14 @@ import 'package:netflixclone/widgets/large_button.dart';
 import 'package:netflixclone/widgets/link_button.dart';
 
 bool _showPassword = false;
+bool _showConfirmPassword = false;
 
 String _fullName = "";
 String _email = "";
 String _password = "";
+String _confirmPassword = "";
+
+bool _policyUpdates = true;
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -32,6 +36,10 @@ class _SignUpState extends State<SignUp> {
       _fullName = "";
       _email = "";
       _password = "";
+      _confirmPassword = "";
+      _showPassword = false;
+      _showConfirmPassword = false;
+      _policyUpdates = true;
     });
   }
 
@@ -123,7 +131,7 @@ class _SignUpState extends State<SignUp> {
                       EdgeInsets.symmetric(horizontal: width * 0.04),
                   suffixIcon: IconButton(
                     icon: Icon(
-                        !_showPassword ? FeatherIcons.eyeOff : FeatherIcons.eye,
+                        _showPassword ? FeatherIcons.eyeOff : FeatherIcons.eye,
                         color: grey3),
                     onPressed: () {
                       setState(() {
@@ -148,35 +156,76 @@ class _SignUpState extends State<SignUp> {
                 },
               ),
               SizedBox(height: height * 0.02),
-              Row(children: [
-                Container(
-                  width: width * 0.4,
-                  height: height * 0.005,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(borderRadius),
-                      color: Colors.grey),
-                  child: Row(
-                    children: [
+              TextField(
+                obscureText: !_showConfirmPassword,
+                decoration: InputDecoration(
+                  fillColor: grey1,
+                  filled: true,
+                  hintText: "Confirm Password",
+                  hintStyle: TextStyle(color: grey3),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: width * 0.04),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _showConfirmPassword
+                            ? FeatherIcons.eyeOff
+                            : FeatherIcons.eye,
+                        color: grey3),
+                    onPressed: () {
+                      setState(() {
+                        _showConfirmPassword = !_showConfirmPassword;
+                      });
+                    },
+                  ),
+                  prefixIcon: null,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(borderRadius),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+                style: TextStyle(
+                  color: white,
+                  fontSize: 15.sp,
+                ),
+                onChanged: (txt) {
+                  setState(() {
+                    _confirmPassword = txt;
+                  });
+                },
+              ),
+              SizedBox(height: height * 0.02),
+              _password.isNotEmpty
+                  ? Row(children: [
                       Container(
-                        width: _password.length < 6 ? width * 0.05 : width * 0.4,
+                        width: width * 0.4,
                         height: height * 0.005,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(borderRadius),
-                            color: _password.length < 6 ? errorRed : successGreen),
+                            color: grey1),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: width * getPasswordStrength()[1],
+                              height: height * 0.005,
+                              decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.circular(borderRadius),
+                                  color: getPasswordStrength()[2]),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(width: width * 0.02),
-                Text(
-                  _password.length < 6 ? "Bad" : "Excellent",
-                  style: TextStyle(
-                    color:  _password.length < 6 ? errorRed : successGreen,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 11.5.sp,
-                  ),
-                ),
-              ]),
+                      SizedBox(width: width * 0.02),
+                      Text(
+                        getPasswordStrength()[0],
+                        style: TextStyle(
+                          color: getPasswordStrength()[2],
+                          fontWeight: FontWeight.w500,
+                          fontSize: 11.5.sp,
+                        ),
+                      ),
+                    ])
+                  : const SizedBox.shrink(),
               SizedBox(height: height * 0.02),
               Text(
                 "Use a minimum of 6 characters (case sensitive) with at least one number of special character.",
@@ -188,12 +237,18 @@ class _SignUpState extends State<SignUp> {
               ),
               SizedBox(height: height * 0.04),
               Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Checkbox(value: true, onChanged: (x) {}),
+                  Checkbox(
+                      value: _policyUpdates,
+                      onChanged: (x) {
+                        setState(() {
+                          _policyUpdates = x!;
+                        });
+                      }),
                   Expanded(
                     child: Text(
-                      "I would like to receive updates, special offers, and other information from Disney+ and The Walt Disney Family of Companies.",
+                      "I would like to receive updates, special offers, and other information from Fliqxify.",
                       style: TextStyle(
                           color: grey3,
                           fontWeight: FontWeight.w500,
@@ -209,9 +264,10 @@ class _SignUpState extends State<SignUp> {
                       Fluttertoast.showToast(msg: "Enter Full Name");
                     } else if (!GetUtils.isEmail(_email)) {
                       Fluttertoast.showToast(msg: "Enter a valid email");
-                    } else if (_password.length < 6) {
-                      Fluttertoast.showToast(
-                          msg: "Password must have atleast 6 characters");
+                    } else if (getPasswordStrength()[1] < 0.3) {
+                      Fluttertoast.showToast(msg: "Create a strong password");
+                    } else if (_password != _confirmPassword) {
+                      Fluttertoast.showToast(msg: "Passwords don't match");
                     } else {
                       Get.to(() => Plans(_fullName, _email, _password));
                     }
@@ -238,5 +294,31 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
     );
+  }
+
+  List getPasswordStrength() {
+    List tooShort = ["Too Short", 0.1, Colors.red];
+    List weak = ["Weak", 0.2, Colors.orange[800]];
+    List strong = ["Strong", 0.3, Colors.green];
+    List great = ["Great", 0.4, Colors.green];
+
+    if (_password.length < 6) {
+      return tooShort;
+    }
+
+    RegExp numReg = RegExp(r".*[0-9].*");
+    RegExp letterReg = RegExp(r".*[A-Za-z].*");
+    RegExp specialReg = RegExp(r'[!@#$%^&*(),.?":{}|<>]');
+    if (!numReg.hasMatch(_password) ||
+        !letterReg.hasMatch(_password) ||
+        !specialReg.hasMatch(_password)) {
+      return weak;
+    }
+
+    if (_password.length <= 10) {
+      return strong;
+    }
+
+    return great;
   }
 }
