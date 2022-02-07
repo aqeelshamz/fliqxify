a7ee3257602b757e32d724671152a75d06143283
Aqeel Shamsudheen <aqeelten@gmail.com>
2022-02-08 00:14:48 +0530

    adminpanel ux improved

--- adminpanel/src/App.js
+++ adminpanel/src/App.js
@@ -24,7 +24,30 @@ function App() {
       .catch((err) => {});
   }
 
+  const [searchResults, setSearchResults] = useState([]);
+
+  function searchMovie(keyword) {
+    const config = {
+      url:
+        "https://api.themoviedb.org/3/search/movie?api_key=3794a566770835ffed011b687794a132&language=en-US&page=1&include_adult=false&query=" +
+        keyword,
+      method: "get",
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(response.data.results);
+        setSearchResults(response.data.results);
+      })
+      .catch((err) => {});
+  }
+
   const uploadMovie = (e) => {
+    if (movieId === "") {
+      alert("Enter Movie ID");
+      window.location.reload();
+      return;
+    }
 
     const data = new FormData();
     data.append("file", e.target.files[0]);
@@ -47,16 +70,23 @@ function App() {
     <div className="App">
       <h1>Fliqxify Admin Panel</h1>
       <h2>Upload Movie</h2>
-      <input type="file" onChange={uploadMovie} />
-      <p>Movie ID</p>
+      <br />  
+      <p>Enter Movie ID:</p>
       <input
         type="text"
         value={movieId}
         onChange={(e) => {
           setMovieId(e.target.value);
         }}
-      />
-      {movies.map((item, index) => {
+      /><br/><br/>
+      <p>Select Movie File to Start Upload:</p>
+      <input type="file" onChange={uploadMovie} />
+      <br />
+      <br />
+      <br />
+      <p>Search movies for Movie ID:</p>
+      <input type="text" onChange={(e) => searchMovie(e.target.value)} />
+      {searchResults.map((item, index) => {
         return (
           <p>
             {item.id} - {item.title}
