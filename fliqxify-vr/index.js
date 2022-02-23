2f0de1d3932cddeac858238af30deaab120e9dc8
Aqeel Shamsudheen <aqeelten@gmail.com>
2022-02-23 07:44:22 +0530

    scene rotation done, vrmode done

--- fliqxify-vr/index.js
+++ fliqxify-vr/index.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-360";
 
 export default class fliqxify_vr extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
+          <Text style={styles.greeting}>Welcome to Fliqxify VR</Text>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
   },
   greetingBox: {
+    height: "100%",
+    width: "100%",
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
     borderWidth: 2,
   },
   greeting: {
@@ -40,4 +35,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('fliqxify_vr', () => fliqxify_vr);
+AppRegistry.registerComponent("fliqxify_vr", () => fliqxify_vr);
