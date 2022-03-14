08c7e6bb719411d5534eb7b6dd206715c8af347f
Aqeel Shamsudheen <aqeelten@gmail.com>
2022-03-14 11:52:28 +0530

    backend - logs

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -285,6 +285,7 @@ router.post("/get-download-url", validate, async (req, res)=>{
     const data = await schema.validateAsync(req.body);
     const movie = await Movie.findOne({movieId: data.movieId});
     var movieData = await axios.get(`https://api.themoviedb.org/3/movie/${data.movieId}?api_key=3794a566770835ffed011b687794a132&language=en-US`);
+    console.log(movieData)
     if(!movie) return res.status(404).send("File not found");
     const url = "https://fliqxify-backend.aqeelshamz.com/" + movie.movieFile;
     return res.send({
