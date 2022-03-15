e86c12b21cbe6f3f6253859688ad3432c9a6b885
Aqeel Shamsudheen <aqeelten@gmail.com>
2022-03-15 12:19:58 +0530

    vr update

--- fliqxify-vr/client.js
+++ fliqxify-vr/client.js
@@ -2,6 +2,9 @@
 // If you want to modify your application's content, start in "index.js"
 
 import {ReactInstance} from 'react-360-web';
+import SimpleRaycaster from 'simple-raycaster';
+import WebVRPolyfill from "webvr-polyfill";
+const polyfill = new WebVRPolyfill();
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,16 +13,25 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
+  console.log(r360._cameraPosition)
+
+  console.log(r360._cameraPosition)
+  // r360.scene.rotateY(210)
   r360.scene.rotateY(-1.5)
+
+
+  var surf = r360.getDefaultSurface();
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
     r360.createRoot('fliqxify_vr', { /* initial props */ }),
-    r360.getDefaultSurface()
+    surf
   );
 
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('dark_theatre.jpg'));
+  r360.controls.clearRaycasters();
+  r360.controls.addRaycaster(SimpleRaycaster);
 }
 
 window.React360 = {init};
